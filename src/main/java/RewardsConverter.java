7f171570faa98cfa20b76a222bacfc2b5eaf1978
Varsha Shubhashri M <21varsha.shri.m@gmail.com>
2024-11-23 22:12:50 +0530

    added class rewardValue

--- src/main/java/RewardsConverter.java
+++ src/main/java/RewardsConverter.java
@@ -1,5 +1,16 @@
 import java.util.Scanner;
+class RewardValue {
+    private static final double CASH_TO_MILES_RATE = 0.1; // Example rate: $1 = 0.1 miles
+    private double cashValue;
 
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+    }
+
+    public double getMilesValue() {
+        return cashValue * CASH_TO_MILES_RATE;
+    }
+}
 public class RewardsConverter {
     public static void main(String[] args) {
         var scanner = new Scanner(System.in);
@@ -17,4 +28,5 @@ public class RewardsConverter {
         var rewardsValue = new RewardValue(cashValue);
         System.out.println("$" + input_value + " is worth " + rewardsValue.getMilesValue() + " miles");
     }
+
 }
